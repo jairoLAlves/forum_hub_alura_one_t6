bf7efb0e5ece0f9d1844815e7efebea80416276e
jairo laranjeira alves <jairoauves8@gmail.com>
2024-06-19 08:56:50 -0300

    first commit

---
+++ src/test/java/br/com/th4mz0/forum_hub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.th4mz0.forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
